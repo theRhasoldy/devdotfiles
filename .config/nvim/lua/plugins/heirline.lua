30192830ae2f4ad06c0880539cf9e7135a79c30c
Nour El Din Shady <rhasoldy@gmail.com>
2023-11-24 14:01:55 +0200

    seperated branch name from changes in heirline

--- .config/nvim/lua/plugins/heirline.lua
+++ .config/nvim/lua/plugins/heirline.lua
@@ -101,6 +101,16 @@ return {
           hl = { fg = colors.purple, bold = true },
         },
         -- You could handle delimiters, icons and counts similar to Diagnostics
+      }
+
+      local GitChanges = {
+        condition = conditions.is_git_repo,
+        init = function(self)
+          self.status_dict = vim.b.gitsigns_status_dict
+          self.has_changes = self.status_dict.added ~= 0
+            or self.status_dict.removed ~= 0
+            or self.status_dict.changed ~= 0
+        end,
         {
           condition = function(self)
             return self.has_changes
@@ -330,6 +340,7 @@ return {
           Diagnostics,
 
           Align,
+          GitChanges,
           Space,
           FileNameBlock,
         },
