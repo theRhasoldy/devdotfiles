416d704215f8a4e6c89823040f411cebbf4d0ef1
Nour El Din Shady <rhasoldy@gmail.com>
2024-07-06 05:06:28 +0300

    added plugins for pairs

---
+++ .config/nvim/lua/plugins/pairs.lua
@@ -0,0 +1,37 @@
+return {
+  {
+    "andymass/vim-matchup",
+    event = "VeryLazy",
+    config = function()
+      vim.g.matchup_matchparen_offscreen = { method = "status_manual" }
+      vim.g.matchup_surround_enabled = 1
+      vim.g.matchup_transmute_enabled = 1
+      vim.g.matchup_matchparen_deferred = 1
+      vim.g.matchup_matchparen_hi_surround_always = 1
+    end,
+  },
+  {
+    "windwp/nvim-autopairs",
+    event = "InsertEnter",
+    opts = {
+      check_ts = true,
+      fast_wrap = {
+        highlight = "Comment",
+        highlight_grey = "Comment",
+      },
+    },
+  },
+  {
+    "windwp/nvim-ts-autotag",
+    event = { "BufReadPre", "BufNewFile" },
+    config = function(_, opts)
+      require("nvim-ts-autotag").setup({
+        opts = {
+          enable_close = true,
+          enable_rename = true,
+          enable_close_on_slash = false,
+        },
+      })
+    end,
+  },
+}
