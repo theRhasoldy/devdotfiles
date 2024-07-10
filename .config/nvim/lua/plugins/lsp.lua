e8e3ce52bdf3b2ab3041ff1c9f09ae48533bfdbc
Nour El Din Shady <rhasoldy@gmail.com>
2024-07-11 00:51:03 +0300

    use telescope for definintion

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -42,7 +42,7 @@ return {
   {
     "neovim/nvim-lspconfig",
     dependencies = {
-        "williamboman/mason-lspconfig.nvim",
+      "williamboman/mason-lspconfig.nvim",
     },
     config = function()
       local ok, lsp = pcall(require, "lspconfig")
@@ -189,7 +189,9 @@ return {
           vim.keymap.set("n", "K", "<cmd>lua vim.lsp.buf.hover()<cr>", opts)
 
           -- go to
-          vim.keymap.set("n", "gd", "<cmd>lua vim.lsp.buf.definition()<cr>", opts)
+          vim.keymap.set("n", "gd", function()
+            require("telescope.builtin").lsp_definitions({ reuse_win = true })
+          end, opts)
           vim.keymap.set("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<cr>", opts)
           vim.keymap.set("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<cr>", opts)
           vim.keymap.set("n", "gt", "<cmd>lua vim.lsp.buf.type_definition()<cr>", opts)
