0ffe4075cc796fa00fee330ca5d644fb15bdb7b4
Nour El Din Shady <rhasoldy@gmail.com>
2024-07-11 03:11:56 +0300

    created tools and added which-key, diff-view and early-retirement

---
+++ .config/nvim/lua/plugins/tools.lua
@@ -0,0 +1,63 @@
+return {
+  {
+    "chrisgrieser/nvim-early-retirement",
+    event = "VeryLazy",
+    opts = {
+      ignoreSpecialBuftypes = false,
+    },
+  },
+  {
+    "folke/which-key.nvim",
+    event = "VeryLazy",
+    init = function()
+      vim.o.timeout = true
+      vim.o.timeoutlen = 300
+    end,
+    opts = {
+      key_labels = {
+        ["<space>"] = "SPC",
+        ["<CR>"] = "RET",
+        ["<tab>"] = "TAB",
+      },
+      window = {
+        border = "single",
+      },
+    },
+  },
+  {
+    "sindrets/diffview.nvim",
+    cmd = { "DiffviewOpen", "DiffviewFileHistory" },
+    opts = {
+      enhanced_diff_hl = true,
+      view = {
+        default = {
+          layout = "diff2_horizontal",
+          winbar_info = true,
+        },
+        merge_tool = {
+          -- Config for conflicted files in diff views during a merge or rebase.
+          layout = "diff4_mixed",
+          disable_diagnostics = false,
+          winbar_info = false,
+        },
+        file_history = {
+          -- Config for changed files in file history views.
+          layout = "diff2_horizontal",
+          winbar_info = false,
+        },
+      },
+    },
+    keys = {
+      {
+        "<leader>gd",
+        "<cmd>DiffviewOpen<CR>",
+        desc = "Open diff view window",
+      },
+      {
+        "<leader>gh",
+        "<cmd>DiffviewFileHistory<CR>",
+        desc = "Open file history view window",
+      },
+    },
+  },
+}
