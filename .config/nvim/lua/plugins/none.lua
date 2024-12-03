163ebe69f0526a5f13410f14fbf70f4ea60366b3
Nour El Din Shady <rhasoldy@gmail.com>
2024-12-03 23:49:11 +0200

    fixed format on save

--- .config/nvim/lua/plugins/none.lua
+++ .config/nvim/lua/plugins/none.lua
@@ -1,43 +1,5 @@
 local utils = require("config.utils")
 
-local async_formatting = function(bufnr)
-  bufnr = bufnr or vim.api.nvim_get_current_buf()
-
-  vim.lsp.buf_request(
-    bufnr,
-    "textDocument/formatting",
-    vim.lsp.util.make_formatting_params({}),
-    function(err, res)
-      if err then
-        local err_msg = type(err) == "string" and err or err.message
-        vim.notify("formatting: " .. err_msg, vim.log.levels.WARN)
-        return
-      end
-
-      -- don't apply results if buffer is unloaded or has been modified
-      if
-        not vim.api.nvim_buf_is_loaded(bufnr)
-        or vim.api.nvim_buf_get_option(bufnr, "modified")
-      then
-        return
-      end
-
-      if res then
-        vim.lsp.buf.format({
-          async = true,
-          bufnr = bufnr,
-          filter = function(client)
-            return client.name == "null-ls"
-          end,
-        })
-        vim.api.nvim_buf_call(bufnr, function()
-          vim.cmd("silent noautocmd update")
-        end)
-      end
-    end
-  )
-end
-
 local augroup = utils.create_group("LspFormatting", {})
 
 return {
@@ -69,7 +31,9 @@ return {
           group = augroup,
           buffer = bufnr,
           callback = function()
-            async_formatting(bufnr)
+            vim.lsp.buf.format({
+              async = true,
+            })
           end,
         })
       end,
